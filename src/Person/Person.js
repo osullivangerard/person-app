51acdee6320db5db47ba1a81d0b1a9fea80a1502
Gerard O Sullivan <osullivangerard@gmail.com>
2018-04-15 12:55:14 +0100

    First commit

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>Name: {props.name}, Age: {props.age}</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+}
+
+export default person;
