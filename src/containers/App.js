936738aec79894f6ace51b95b8f2299a536bb0f4
Gerard O Sullivan <osullivangerard@gmail.com>
2018-04-17 18:05:48 +0100

    added lifecycle methods for component updates in App

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,6 +21,19 @@ class App extends Component {
     console.log('[App.js] Inside componentWillUnmount()');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
   state = {
     persons: [
       { id: 'sds', name: 'Max', age: 28 },
