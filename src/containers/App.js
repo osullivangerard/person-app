696608b1f5dadc02b934926a675346a68bab55b3
Gerard O Sullivan <osullivangerard@gmail.com>
2018-04-18 14:48:00 +0100

    added a WithClass hoc

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -81,7 +82,7 @@ class App extends PureComponent {
     }
 
     return (
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
           <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
           <Cockpit
             appTitle={this.props.title}
@@ -89,7 +90,7 @@ class App extends PureComponent {
             persons={this.state.persons}
             clicked={this.togglePersonsHandler} />
           {persons}
-        </div>
+        </WithClass>
     );
   }
 }
